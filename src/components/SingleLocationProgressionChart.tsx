9f16866e6bb55bd91f53728fe45e04ecb28cb5d7
Evrim P <evrimfeyyaz@gmail.com>
2020-04-05 15:50:05 +0400

    Tell user that there is no data to show when it is the case

--- src/components/SingleLocationProgressionChart.tsx
+++ src/components/SingleLocationProgressionChart.tsx
@@ -31,38 +31,54 @@ const SingleLocationProgressionChart: FunctionComponent<SingleLocationProgressio
     exceedingPropertyText = 'deaths';
   }
 
+  const chart = (
+    <ResponsiveContainer width='100%' height={400}>
+      <ComposedChart width={600} height={400} data={data}
+                     margin={{ top: 20, right: 30, bottom: 40, left: 30 }}>
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis>
+          <Label value={`Number of days since ${exceedingPropertyText} exceeded ${exceedingValue}`}
+                 position='bottom' offset={10} />
+        </XAxis>
+
+        <YAxis
+          yAxisId="left"
+          label={{ value: 'Cases', angle: -90, position: 'left', dy: -20, dx: -15 }}
+        />
+        <YAxis
+          yAxisId="right"
+          orientation="right"
+          domain={[0, dataMax => dataMax * 5]}
+          label={{ value: 'Deaths', angle: 90, position: 'right', dy: -25, dx: 5 }}
+        />
+        <Tooltip content={SingleLocationProgressionTooltip} />
+        <Legend align='center' verticalAlign='top' height={45} />
+        <Bar dataKey='newConfirmed' yAxisId='left' fill={COLORS.newConfirmed} name='New Cases' />
+        <Line
+          type='monotone' dataKey='deaths' yAxisId='right'
+          stroke={COLORS.deaths} strokeWidth={3} name='Deaths'
+          dot={false}
+        />
+        <Line
+          type='monotone' yAxisId='left' dataKey="confirmed"
+          stroke={COLORS.confirmed} strokeWidth={3} name='Confirmed Cases'
+          dot={false}
+        />
+      </ComposedChart>
+    </ResponsiveContainer>
+  );
+
   return (
     <>
       <h1 className='h3 mb-3'>COVID-19 Progression: {location}</h1>
       <Card className='shadow-lg border-0' style={{ borderRadius: 15 }}>
         <Card.Body>
-          <ResponsiveContainer width='100%' height={400}>
-            <ComposedChart width={600} height={400} data={data}
-                           margin={{ top: 20, right: 20, bottom: 40, left: 20 }}>
-              <CartesianGrid strokeDasharray="3 3" />
-              <XAxis>
-                <Label value={`Number of days since ${exceedingPropertyText} exceeded ${exceedingValue}`}
-                       position='bottom'
-                       offset={20} />
-              </XAxis>
-              <YAxis yAxisId="left" label={{ value: 'Cases', angle: -90, position: 'left' }} />
-              <YAxis yAxisId="right" orientation="right" domain={[0, dataMax => dataMax * 5]}
-                     label={{ value: 'Deaths', angle: 90, position: 'right' }} />
-              <Tooltip content={SingleLocationProgressionTooltip} />
-              <Legend align='center' verticalAlign='top' height={40} />
-              <Bar dataKey='newConfirmed' yAxisId='left' fill={COLORS.newConfirmed} name='New Cases' />
-              <Line
-                type='monotone' dataKey='deaths' yAxisId='right'
-                stroke={COLORS.deaths} strokeWidth={3} name='Deaths'
-                dot={false} activeDot={false}
-              />
-              <Line
-                type='monotone' yAxisId='left' dataKey="confirmed"
-                stroke={COLORS.confirmed} strokeWidth={3} name='Confirmed Cases'
-                dot={false} activeDot={false}
-              />
-            </ComposedChart>
-          </ResponsiveContainer>
+          {data.length === 0 && (
+            <h2 className='h6 text-center my-auto'>
+              No data to show.
+            </h2>
+          )}
+          {data.length > 0 && chart}
         </Card.Body>
       </Card>
     </>
