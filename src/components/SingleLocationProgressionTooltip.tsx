2e32ff948ce50d7dcf2a69dfe60307c57a9e9446
Evrim P <evrimfeyyaz@gmail.com>
2020-04-08 16:09:52 +0400

    Improve the look of the chart tooltip and make it smaller

--- src/components/SingleLocationProgressionTooltip.tsx
+++ src/components/SingleLocationProgressionTooltip.tsx
@@ -19,40 +19,42 @@ const SingleLocationProgressionTooltip: FunctionComponent<SingleLocationProgress
   const { confirmed, newConfirmed, deaths, recovered, date } = payload[0].payload;
 
   return (
-    <Card className='shadow-sm rounded-lg'>
-      <Card.Body className='rounded-lg'>
-        <Card.Title className='text-center'>
-          {prettifyMDYDate(date)}
-        </Card.Title>
-        <Card.Text>
-          <Table borderless size='sm'>
-            <tbody>
-            <tr>
-              <td style={{ backgroundColor: COLORS.confirmed, width: 5, borderRadius: 5 }} />
-              <th>Confirmed Cases</th>
-              <td className='text-right'>{confirmed}</td>
-            </tr>
-            <tr style={{ height: 5 }} />
-            <tr>
-              <td style={{ backgroundColor: COLORS.newConfirmed, width: 5, borderRadius: 5 }} />
-              <th>New Cases</th>
-              <td className='text-right'>{newConfirmed}</td>
-            </tr>
-            <tr style={{ height: 5 }} />
-            <tr>
-              <td style={{ backgroundColor: COLORS.recovered, width: 5, borderRadius: 5 }} />
-              <th>Recovered Cases</th>
-              <td className='text-right'>{recovered ?? 'n/a'}</td>
-            </tr>
-            <tr style={{ height: 5 }} />
-            <tr>
-              <td style={{ backgroundColor: COLORS.deaths, width: 5, borderRadius: 5 }} />
-              <th>Deaths</th>
-              <td className='text-right'>{deaths ?? 'n/a'}</td>
-            </tr>
-            </tbody>
-          </Table>
-        </Card.Text>
+    <Card className='shadow rounded-lg'>
+      <Card.Body className='rounded-lg pt-3 pb-2 px-4'>
+        <small style={{lineHeight: 1}}>
+          <Card.Title className='text-center'>
+            {prettifyMDYDate(date)}
+          </Card.Title>
+          <Card.Text>
+            <Table borderless size='sm'>
+              <tbody>
+              <tr>
+                <td style={{ backgroundColor: COLORS.confirmed, width: 5, borderRadius: 5 }} />
+                <th>Confirmed Cases</th>
+                <td className='text-right'>{confirmed}</td>
+              </tr>
+              <tr style={{ height: 5 }} />
+              <tr>
+                <td style={{ backgroundColor: COLORS.newConfirmed, width: 5, borderRadius: 5 }} />
+                <th>New Cases</th>
+                <td className='text-right'>{newConfirmed}</td>
+              </tr>
+              <tr style={{ height: 5 }} />
+              <tr>
+                <td style={{ backgroundColor: COLORS.recovered, width: 5, borderRadius: 5 }} />
+                <th>Recovered Cases</th>
+                <td className='text-right'>{recovered ?? 'n/a'}</td>
+              </tr>
+              <tr style={{ height: 5 }} />
+              <tr>
+                <td style={{ backgroundColor: COLORS.deaths, width: 5, borderRadius: 5 }} />
+                <th>Deaths</th>
+                <td className='text-right'>{deaths ?? 'n/a'}</td>
+              </tr>
+              </tbody>
+            </Table>
+          </Card.Text>
+        </small>
       </Card.Body>
     </Card>
   );
