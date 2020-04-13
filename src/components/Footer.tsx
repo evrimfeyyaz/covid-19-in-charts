03527952b2268413572ef9d46c3e6029c752fe75
Evrim P <evrimfeyyaz@gmail.com>
2020-04-13 21:58:36 +0400

    Fix an error that caused the background color set in styles.scss to not take effect

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,6 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import { Link } from 'react-router-dom';
 import { EXTERNAL_LINKS, ROUTE_PATHS } from '../constants';
-import Nav from 'react-bootstrap/Nav';
 
 const Footer: FunctionComponent = () => {
   return (
