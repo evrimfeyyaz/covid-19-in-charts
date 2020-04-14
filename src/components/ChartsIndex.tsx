6d2bcb5b36fd00321ddedad87ac9cc3ff97eef14
Evrim P <evrimfeyyaz@gmail.com>
2020-04-14 19:44:39 +0400

    Add a home page

--- src/components/ChartsIndex.tsx
+++ src/components/ChartsIndex.tsx
@@ -3,27 +3,31 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
+import { Link } from 'react-router-dom';
+import { ROUTE_PATHS } from '../constants';
 
 const ChartsIndex: FunctionComponent = () => {
   return (
     <Container>
       <Row>
-        <Col lg={4}>
-          <Card>
+        <Col sm={6} lg={4}>
+          <Card className='my-2'>
+            <Card.Img variant="top" src={require('../images/cases-recoveries-and-deaths.jpg')} />
             <Card.Body>
               <Card.Title>
                 Cases, Recoveries and Deaths
               </Card.Title>
               <Card.Text>
                 See the number of confirmed cases, new cases, recoveries and deaths.
+                <Link to={ROUTE_PATHS.casesInLocation} className='stretched-link' />
               </Card.Text>
             </Card.Body>
           </Card>
         </Col>
 
-        <Col lg={4}>
-          <Card>
-            <Card.Img variant="top" src={require('../images/more-charts-to-come.png')} />
+        <Col sm={6} lg={4}>
+          <Card className='my-2'>
+            <Card.Img variant="top" src={require('../images/more-charts-to-come.jpg')} />
             <Card.Body>
               <Card.Title>
                 More Visualizations to Come
