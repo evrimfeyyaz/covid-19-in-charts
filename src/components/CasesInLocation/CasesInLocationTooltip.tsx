81b07ebb111a911f82a25144e9b143b08045dcb1
Evrim P <evrimfeyyaz@gmail.com>
2020-04-17 21:06:06 +0400

    Fix a component that caused an invalid HTML markup structure

--- src/components/CasesInLocation/CasesInLocationTooltip.tsx
+++ src/components/CasesInLocation/CasesInLocationTooltip.tsx
@@ -21,41 +21,39 @@ const CasesInLocationTooltip: FunctionComponent<CasesInLocationTooltip> = ({ pay
 
   return (
     <Card className='shadow rounded-lg'>
-      <Card.Body className='rounded-lg pt-3 pb-2 px-4'>
-        <small style={{ lineHeight: 1 }}>
-          <Card.Title className='text-center'>
-            {prettifyMDYDate(date)}
-          </Card.Title>
-          <Card.Text>
-            <Table borderless size='sm'>
-              <tbody>
-              <tr>
-                <td style={{ backgroundColor: COLORS.confirmed, width: 5, borderRadius: 5 }} />
-                <th>Confirmed Cases</th>
-                <td className='text-right'>{numToGroupedString(confirmed)}</td>
-              </tr>
-              <tr style={{ height: 5 }} />
-              <tr>
-                <td style={{ backgroundColor: COLORS.newConfirmed, width: 5, borderRadius: 5 }} />
-                <th>New Cases</th>
-                <td className='text-right'>{numToGroupedString(newConfirmed)}</td>
-              </tr>
-              <tr style={{ height: 5 }} />
-              <tr>
-                <td style={{ backgroundColor: COLORS.recovered, width: 5, borderRadius: 5 }} />
-                <th>Recovered Cases</th>
-                <td className='text-right'>{recovered != null ? numToGroupedString(recovered) : 'n/a'}</td>
-              </tr>
-              <tr style={{ height: 5 }} />
-              <tr>
-                <td style={{ backgroundColor: COLORS.deaths, width: 5, borderRadius: 5 }} />
-                <th>Deaths</th>
-                <td className='text-right'>{deaths != null ? numToGroupedString(deaths) : 'n/a'}</td>
-              </tr>
-              </tbody>
-            </Table>
-          </Card.Text>
-        </small>
+      <Card.Body className='rounded-lg pt-3 pb-2 px-4 small' style={{ lineHeight: 1 }}>
+        <Card.Title className='text-center'>
+          {prettifyMDYDate(date)}
+        </Card.Title>
+        <Card.Text as='div'>
+          <Table borderless size='sm'>
+            <tbody>
+            <tr>
+              <td style={{ backgroundColor: COLORS.confirmed, width: 5, borderRadius: 5 }} />
+              <th>Confirmed Cases</th>
+              <td className='text-right'>{numToGroupedString(confirmed)}</td>
+            </tr>
+            <tr style={{ height: 5 }} />
+            <tr>
+              <td style={{ backgroundColor: COLORS.newConfirmed, width: 5, borderRadius: 5 }} />
+              <th>New Cases</th>
+              <td className='text-right'>{numToGroupedString(newConfirmed)}</td>
+            </tr>
+            <tr style={{ height: 5 }} />
+            <tr>
+              <td style={{ backgroundColor: COLORS.recovered, width: 5, borderRadius: 5 }} />
+              <th>Recovered Cases</th>
+              <td className='text-right'>{recovered != null ? numToGroupedString(recovered) : 'n/a'}</td>
+            </tr>
+            <tr style={{ height: 5 }} />
+            <tr>
+              <td style={{ backgroundColor: COLORS.deaths, width: 5, borderRadius: 5 }} />
+              <th>Deaths</th>
+              <td className='text-right'>{deaths != null ? numToGroupedString(deaths) : 'n/a'}</td>
+            </tr>
+            </tbody>
+          </Table>
+        </Card.Text>
       </Card.Body>
     </Card>
   );
