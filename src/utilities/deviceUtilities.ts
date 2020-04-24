73e66a8735df5ce41d2837bb1049bcf7a1cb1b35
Evrim P <evrimfeyyaz@gmail.com>
2020-04-24 23:13:48 +0400

    Use the built-in date picker when it is supported

---
+++ src/utilities/deviceUtilities.ts
@@ -0,0 +1,10 @@
+// From: https://stackoverflow.com/questions/10193294/how-can-i-tell-if-a-browser-supports-input-type-date#comment28536897_10199306
+export function isDateInputSupported() {
+  const el = document.createElement('input');
+  const notADateValue = 'not-a-date';
+
+  el.setAttribute('type', 'date');
+  el.setAttribute('value', notADateValue);
+
+  return el.value !== notADateValue;
+}
