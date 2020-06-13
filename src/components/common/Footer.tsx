f5d48a9b5e6671629adf7cb7a4c7f523fd7a8fd7
Evrim P <evrimfeyyaz@gmail.com>
2020-06-13 15:30:02 +0400

    Fix the issue of the footer not rendering properly below medium sized screens

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -1,7 +1,9 @@
 import React, { FunctionComponent } from "react";
+import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import Row from "react-bootstrap/Row";
 import packageJson from "../../../package.json";
 import { EXTERNAL_LINKS, ROUTE_PATHS } from "../../constants";
 
@@ -10,27 +12,33 @@ import { EXTERNAL_LINKS, ROUTE_PATHS } from "../../constants";
  */
 export const Footer: FunctionComponent = () => {
   return (
-    <Navbar bg="dark" variant="dark" as="footer" className="pt-4 pb-5">
-      <Container>
-        <Nav as="ul" bg={"dark"}>
-          <Nav.Item as="li" className="mr-4">
-            <Nav.Link href={ROUTE_PATHS.home}>Home</Nav.Link>
-          </Nav.Item>
-          <Nav.Item as="li" className="mr-4">
-            <Nav.Link href={EXTERNAL_LINKS.gitHubRepo}>GitHub</Nav.Link>
-          </Nav.Item>
-          <Nav.Item as="li">
-            <Nav.Link href={ROUTE_PATHS.about}>About</Nav.Link>
-          </Nav.Item>
-        </Nav>
+    <Navbar bg="dark" as="footer" variant="dark" className="pt-4 pb-4">
+      <Container className="w-100 flex-wrap justify-content-center">
+        <Row className="w-100 align-items-baseline">
+          <Col xs={12} md={6}>
+            <Nav as="ul" bg={"dark"} className="justify-content-center justify-content-md-start">
+              <Nav.Item as="li" className="mr-4">
+                <Nav.Link href={ROUTE_PATHS.home}>Home</Nav.Link>
+              </Nav.Item>
+              <Nav.Item as="li" className="mr-4">
+                <Nav.Link href={EXTERNAL_LINKS.gitHubRepo}>GitHub</Nav.Link>
+              </Nav.Item>
+              <Nav.Item as="li">
+                <Nav.Link href={ROUTE_PATHS.about}>About</Nav.Link>
+              </Nav.Item>
+            </Nav>
+          </Col>
 
-        <div className="text-light">
-          Created by{" "}
-          <a href={EXTERNAL_LINKS.authorWebsite} className="text-warning">
-            Evrim Persembe
-          </a>
-          <span className="text-secondary ml-4">v{packageJson.version}</span>
-        </div>
+          <Col xs={12} md={6}>
+            <div className="text-light text-center text-md-right mt-5 mt-md-0">
+              Created by{" "}
+              <a href={EXTERNAL_LINKS.authorWebsite} className="text-warning">
+                Evrim Persembe
+              </a>
+              <span className="text-secondary ml-4">v{packageJson.version}</span>
+            </div>
+          </Col>
+        </Row>
       </Container>
     </Navbar>
   );
