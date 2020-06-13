8fef6c4c0b8a2c411537f6d0dfc8dcc6dd6191a5
Evrim P <evrimfeyyaz@gmail.com>
2020-06-13 16:16:12 +0400

    Update the sitemap

--- src/sitemapGenerator.js
+++ src/sitemapGenerator.js
@@ -8,8 +8,5 @@ const writeStream = createWriteStream("./public/sitemap.xml");
 sitemap.pipe(writeStream);
 
 sitemap.write({ url: "/", changefreq: "daily" });
-sitemap.write({ url: "/cases-recoveries-deaths", changefre: "daily", priority: 0.6 });
-sitemap.write({ url: "/daily-numbers", changefre: "daily", priority: 0.6 });
-sitemap.write({ url: "/location-comparison", changefre: "daily", priority: 0.6 });
 sitemap.write({ url: "/about", changefre: "weekly" });
 sitemap.end();
